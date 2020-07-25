5479435615292c96ea15f7f6c961d33e5eab2057
quiet396 <kohei.ingenious3.fx@hotmail.com>
2020-07-25 18:03:39 +0900

    [mod] change route

--- RailsDeviceApp/config/routes.rb
+++ RailsDeviceApp/config/routes.rb
@@ -1,21 +1,41 @@
 Rails.application.routes.draw do
-  get 'blogs/index'
-  get 'blogs/genre'
-  get 'blogs/show'
+  gget 'blogs/index'
+  get 'blogs', to: 'blogs#index'
+  get 'blogs/:page', to: 'blogs#index'
+
+  get 'blogs/genre/:id', to: 'blogs#genre'
+  get 'blogs/genre/:id/:page', to: 'blogs#genre'
+
+  get 'blogs/show/:id', to: 'blogs#show'
+
   get 'blogposts/index'
+  get 'blogposts', to: 'blogposts#index'
+
+  get 'blogposts/delete/:id', to: 'blogposts#delete'
+  post 'blogposts/delete', to: 'blogposts#delete'
+  post 'blogposts/delete/:id', to: 'blogposts#delete'
+
   get 'blogposts/add'
-  get 'blogposts/edit'
+  post 'blogposts/add'
+
+  get 'blogposts/:id', to: 'blogposts#edit'
+  patch 'blogposts/:id', to: 'blogposts#edit'
+
   get 'blogposts/delete'
+
   get 'bloggenres/index'
+  get 'bloggenres', to: 'bloggenres#index'
+
   get 'bloggenres/add'
-  get 'bloggenres/edit'
+  post 'bloggenres/add'
+
+  get 'bloggenres/:id', to: 'bloggenres#edit'
+  patch 'bloggenres/:id', to: 'bloggenres#edit'
+
   get 'blogconfigs/index'
+  get 'blogconfigs', to: 'blogconfigs#index'
+
   get 'blogconfigs/edit'
-  resources :board_messages
-  resources :board_users
-  resources :data_pages
-  get 'hello/index'
-  get 'hello/login_check'
-  devise_for :accounts
+  patch 'blogconfigs/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
